41259dbcabeb756f182ce848a0bbd73e0068177d
Melike <mztapci@gmail.com>
2024-05-24 15:04:32 +0300

    navbar corrected

--- send_message.php
+++ send_message.php
@@ -1,17 +1,54 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-   
-    $fullname = $_POST['fullname'];
-    $email = $_POST['email'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
-
-    
-    echo "Message sent successfully!";
-
+//Connect with DB
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "group2";
+// Create connection
+$mysqli = new mysqli($servername, $username,
+                $password, $dbname);
+ 
+// Checking for connections
+if ($mysqli->connect_error) {
+    die('Connect Error (' .
+    $mysqli->connect_errno . ') '.
+    $mysqli->connect_error);
 }
-
-
+ 
+// SQL query to select data from database
+$sql = " SELECT * FROM messages WHERE fullname= 1";
+$result = $mysqli->query($sql);
+$mysqli->close();
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title> Courses </title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='coursesGall_styles.css'>
+
+</head>
+
+<body>
+
+    <div>
+        Subject : <?php echo $rows["subject"]; ?>
+        <br>
+        Message : <?php echo $rows["message"]; ?>
+        <br>
+        Name : <?php echo $rows["fullname"]; ?>
+        <br>
+        Email : <?php echo $rows["email"]; ?>
+    </div>
+
+</body>
+</html>
+
+
+
+
 
